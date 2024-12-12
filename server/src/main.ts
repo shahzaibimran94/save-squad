4ada1e264cc1705a9d1fab301cd1abc536e78d75
Shahzaib Imran <shahzaib.imran94@gmail.com>
2024-12-12 22:11:42 +0000

    added swagger documentation to list APIs

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { LoggingService } from './logger/logger.service';
 import { AllExceptionsFilter } from './logger/logger.exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -16,6 +17,20 @@ async function bootstrap() {
   );
 
   app.set('trust proxy', 1);
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Save Squad REST APIs')                 // API title
+    .setDescription('APIs designed to manage customer payments, enabling secure and efficient processing of transactions, including payment method management.')  // API description
+    .setVersion('1.0')                  // Version
+    // .addTag('example')                  // Optional tag for grouping
+    .build();
+
+  // Create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger UI
+  SwaggerModule.setup('api-docs', app, document); // Swagger UI will be available at /api-docs
   
   await app.listen(process.env.PORT ?? 3000);
 }
